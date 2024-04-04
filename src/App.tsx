819911f97ce3dd2e8f65402dbca1c7ccb2c44c1c
Deluxan Mariathasan <deluxan.mariathasan@gmail.com>
2024-04-04 22:13:22 +0530

    theming using rn elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import {SafeAreaView, ScrollView, StatusBar, Text, View} from 'react-native';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import {ThemeProvider} from '@rneui/themed';
+import {theme} from './rn-elements/theme/theme';
 
 function App(): React.JSX.Element {
   return (
-    <SafeAreaView>
-      <StatusBar />
-      <ScrollView contentInsetAdjustmentBehavior="automatic">
-        <View>
-          <Text>App Component</Text>
-          <Icon name="rocket" />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <ThemeProvider theme={theme}>
+      <SafeAreaView>
+        <StatusBar />
+        <ScrollView contentInsetAdjustmentBehavior="automatic">
+          <View>
+            <Text>App Component</Text>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </ThemeProvider>
   );
 }
 
---
+++ src/rn-elements/theme/theme.ts
@@ -0,0 +1,13 @@
+import {createTheme} from '@rneui/themed';
+
+export const theme = createTheme({
+  lightColors: {
+    primary: '#7210FF',
+    secondary: '#FFC02D',
+  },
+  darkColors: {
+    primary: '#7210FF',
+    secondary: '#FFC02D',
+  },
+  mode: 'dark',
+});
