b0e712df63f457a63e9a6350cb3add75fba323f9
Deluxan Mariathasan <deluxan.mariathasan@gmail.com>
2024-04-06 17:40:55 +0530

    coverage ignore files

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,9 @@ module.exports = {
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|react-redux|@rneui)',
   ],
   setupFiles: ['<rootDir>/jest.setup.js'],
+  coveragePathIgnorePatterns: [
+    '<rootDir>/src/navigation/bottom-tab/bottom-tab-navigation.tsx',
+    '<rootDir>/src/navigation/stack-nav/stack-navigation.tsx',
+    '<rootDir>/src/App.tsx',
+  ],
 };
